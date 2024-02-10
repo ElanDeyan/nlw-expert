65e533fb6b941ece56b5f3e6fe4e90a9e285ef6e
Elan Deyan <deyancaio83@hotmail.com>
2024-02-10 18:19:33 -0300

    test: implements unittests

---
+++ src/validators/tag_creator_validator_test.py
@@ -0,0 +1,24 @@
+from src.errors.error_types.http_unprocessable_entity_error import (
+    HttpUnprocessableEntityError,
+)
+from src.validators.tag_creator_validator import tag_creator_validator
+
+
+class MockRequest:
+    def __init__(self, json) -> None:
+        self.json = json
+
+
+def test_tag_creator_validator():
+    req = MockRequest(json={"product_code": "123456"})
+    tag_creator_validator(req)
+
+
+def test_tag_creator_validator_raises():
+    req = MockRequest(json={"product_code": 12345})
+
+    try:
+        tag_creator_validator(req)
+        assert False
+    except Exception as exception:
+        assert isinstance(exception, HttpUnprocessableEntityError)
